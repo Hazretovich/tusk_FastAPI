452a0318a968744eccc27e1f5634959e8fa4e632
Hazretovich <azamatgeri-dukov@yandex.ru>
2024-04-01 23:03:31 +0300

    Has been written robot script

--- robot.py
+++ robot.py
@@ -1,1 +1,13 @@
-# initial
+import time
+import sys
+
+if len(sys.argv) > 1:
+    value = sys.argv[1]
+else:
+    value = 0
+working = True
+
+while working:
+    time.sleep(1)
+    print(value)
+    value+=1
