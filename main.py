654653efe7c85e5faf5636bcccf5b8a85189d7dd
Hazretovich <azamatgeri-dukov@yandex.ru>
2024-04-01 23:31:25 +0300

    2 methods have been written, to start and stop the robot.

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import subprocess
+
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Robot"
+)
 
-@app.get("/robot_start")
-def start():
-    return "initial"
+@app.get("/robot/start")
+def robot_start(value: int = 0):
+    global robot
+    robot = subprocess.Popen(['python', 'robot.py', str(value)])
+    return "The robot has started working"
+
+#
+@app.get("/robot/stop")
+def robot_stop():
+    robot.terminate()
+    return "The robot has stoped working"
