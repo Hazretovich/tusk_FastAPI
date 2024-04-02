c4f3980c21cc7b530ab08335023604e5a62486f0
Hazretovich <azamatgeri-dukov@yandex.ru>
2024-04-02 14:03:24 +0300

    Rewrite api method. Instead of two, one method

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import subprocess
-
 from fastapi import FastAPI
 
 app = FastAPI(
     title="Robot"
 )
+is_working = False
 
-@app.get("/robot/start")
+@app.get("/robot")
 def robot_start(value: int = 0):
-    global robot
-    robot = subprocess.Popen(['python', 'robot.py', str(value)])
-    return "The robot has started working"
-
-#
-@app.get("/robot/stop")
-def robot_stop():
-    robot.terminate()
-    return "The robot has stoped working"
+    global is_working
+    if not is_working:
+        global robot
+        robot = subprocess.Popen(['python', 'robot.py', str(value)])
+        is_working = True
+        return "The robot has started working"
+    else:
+        robot.terminate()
+        is_working = False
+        return "The robot has stoped working"
